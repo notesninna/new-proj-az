9e6a807c24e5df4795953488e91831a5a2a669ba
notesninna <notesninna@gmail.com>
2025-03-05 10:26:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -23,9 +23,9 @@ class Config(object):
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET") or "7hZ8Q~9EFp7gy8.IKDZajlSH4SbuCi1UYFgX.bRz"
-     if not CLIENT_SECRET:
-         raise ValueError("Need to define CLIENT_SECRET environment variable")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    if not CLIENT_SECRET:
+        raise ValueError("Need to define CLIENT_SECRET environment variable")
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
